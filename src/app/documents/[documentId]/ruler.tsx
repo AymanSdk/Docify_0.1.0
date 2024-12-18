dcf2c72f1e4a666811b65514f1e55af56790cc89
Aymansdk <ayman.sdk123@outlook.com>
2024-12-18 12:22:05 +0100

    Refactor Ruler component layout and remove redundant ref

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -57,16 +57,13 @@ export const Ruler = () => {
 
   return (
     <div
+      ref={rulerRef}
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
       onMouseLeave={handleMouseUp}
-      ref={rulerRef}
-      className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden"
+      className="w-[816px] mx-auto h-6 border-b border-gray-300 flex items-end relative select-none print:hidden"
     >
-      <div
-        id="ruler-container"
-        className="max-w-[816px] mx-auto w-full h-full relative"
-      >
+      <div id="ruler-container" className="w-full h-full relative">
         <Marker
           position={letfMargin}
           isLeft={true}
