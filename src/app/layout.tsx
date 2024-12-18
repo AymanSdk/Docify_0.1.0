199bca1c2649b03dea3e502e8977d646d8e3bf7b
Aymansdk <ayman.sdk123@outlook.com>
2024-12-18 14:27:40 +0100

    Update layout component to change application name to Docify and enhance metadata description; integrate NuqsAdapter for improved functionality.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+
 import "./globals.css";
 
 const inter = Inter({
@@ -7,8 +9,9 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Typly",
-  description: "A simple, fast, and powerful document editor for everyone 🚀",
+  title: "Docify",
+  description:
+    "Docify is a powerful online document editor for creating, editing, and sharing documents in real time. With seamless collaboration tools, cloud storage, and a user-friendly interface, it's the perfect platform for individuals and teams to work smarter.",
 };
 
 export default function RootLayout({
@@ -18,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NuqsAdapter>{children}</NuqsAdapter>
+      </body>
     </html>
   );
 }
