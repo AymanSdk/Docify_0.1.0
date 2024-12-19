54a27fe6c4cf1abafefac459a03e3a71160d5002
Aymansdk <ayman.sdk123@outlook.com>
2024-12-19 19:19:41 +0100

    Wrap Editor component in Room for improved context management

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,13 +1,9 @@
 import { Editor } from "./editor";
 import { Navbar } from "./navbar";
+import { Room } from "./room";
 import { Toolbar } from "./toolbar";
-interface DocumentIdPageProps {
-  params: Promise<{ documentId: string }>;
-}
 
-const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
-  const { documentId } = await params;
-
+const DocumentIdPage = async () => {
   return (
     <div className="min-h-screen bg-[#FAFBFD] ">
       <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#fafbfd] print:hidden">
@@ -15,7 +11,9 @@ const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
         <Toolbar />
       </div>
       <div className="pt-[114px] print:pt-0">
-        <Editor />
+        <Room>
+          <Editor />
+        </Room>
       </div>
     </div>
   );
